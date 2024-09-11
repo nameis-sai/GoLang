b23c67fd10dbc4391cca3824234031900199401a
nameis-sai <saimohan912@gmail.com>
2024-09-11 19:07:48 +0530

    Go Lang Variables

---
+++ 02_Variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Sai Mohan"
+	fmt.Println(username)
+	fmt.Printf("Type of username :  %T \n ", username)
+
+	var rollNo int = 25
+	fmt.Println(rollNo)
+	fmt.Printf("Type of rollNo :  %T \n ", rollNo)
+
+	var pwAuth bool = true
+	fmt.Println(pwAuth)
+	fmt.Printf("Type of pwAuth :  %T \n ", pwAuth)
+}
