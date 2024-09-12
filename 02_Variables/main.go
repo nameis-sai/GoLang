828b4d7cca426a55ec185fcdb6279c6fea3cb257
nameis-sai <saimohan912@gmail.com>
2024-09-12 12:49:59 +0530

    Variables

--- 02_Variables/main.go
+++ 02_Variables/main.go
@@ -14,4 +14,28 @@ func main() {
 	var pwAuth bool = true
 	fmt.Println(pwAuth)
 	fmt.Printf("Type of pwAuth :  %T \n ", pwAuth)
+
+	//implicit type
+
+	var first = 37
+	fmt.Println(first)
+	fmt.Printf("Type of first : %T \n", first)
+
+	var second = "Sai"
+	fmt.Println(second)
+	fmt.Printf("Type of second : %T \n", second)
+
+	//default values
+
+	var abc string
+	fmt.Println(abc)
+
+	var def int
+	fmt.Println(def)
+
+	var ghi bool
+	fmt.Println(ghi)
+
+	//no var style
+
 }
